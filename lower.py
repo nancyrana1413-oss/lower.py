b9544ca779d1f8cdccbba9518e684f4b30646860
nancyrana1413-oss <nancyrana1413@gmail.com>
2025-11-15 01:34:10 +0530

    Implement case counting function in lower.py
    Add a function to count uppercase and lowercase letters in a string.

---
+++ lower.py
@@ -0,0 +1,18 @@
+
+def count_case(s):
+    lower=0
+    upper=0
+    for ch in s:
+        if ch. islower():
+            lower+=1
+        elif ch .isupper():
+            upper+=1
+    print("uppercase letter:",upper)
+    print("lowercase letter:", lower)
+text=(input("enter the string:"))
+count_case(text)
+          
+                 
+            
+        
+    
